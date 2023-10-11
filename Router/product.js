38bd175150e43b45156c885b1cbbc3a15f35999f
BasZ4ll <bas101207@gmail.com>
2023-10-11 22:09:50 +0700

    update 11/01/2023

--- Router/product.js
+++ Router/product.js
@@ -7,11 +7,12 @@ const { upload } = require('../middleware/upload')
 
 //http://localhost:5000/api/product
 router.get('/product', list)
+//router.get('/product', auth, list)
+//if you want to use middleware, you can use it like this. ""auth""" 
 router.get('/product/:id', read)
 router.post('/product', upload, create)
 router.put('/product/:id', update)
 router.delete('/product/:id', deleted)
-
 
 
 module.exports = router
